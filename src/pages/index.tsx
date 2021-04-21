f83bc401afa11d4ae7d7334fb11fa0f8b0975a05
Natalia <natalia-321s@outlook.com>
2021-04-20 21:35:29 -0300

    Componente Player e consumo de fake api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
-import { Header } from "../components/Header";
-
-export default function Home() {
+export default function Home(props) {
+  
   return (
-    <Header/>
+    <>
+      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
   )
+}
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
