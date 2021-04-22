9ba0edcb6ff789f3a590cfcfb87719f16e52b01f
Natalia <natalia-321s@outlook.com>
2021-04-21 23:36:11 -0300

    Axios e formatação dos dados consumidos da fake api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,26 @@
-export default function Home(props) {
+import { GetStaticProps} from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string;
+  duration: string;
+  durationAsString: string;
+  published_at: string;
+  url: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   
   return (
     <>
@@ -8,13 +30,30 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps(){
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
-
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
   return {
     props: {
-      episodes: data
+      episodes: episodes
     },
     revalidate: 60 * 60 * 8,
   }
