eb9d4f53b44c3eceec6763fa6840762402ee494f
Natalia <natalia-321s@outlook.com>
2021-04-24 20:33:39 -0300

    Estilizando scrollbar e link para home no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR'
+import Link from 'next/link';
 import styles from "./styles.module.scss";
+
 export function Header() {
 
   const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
@@ -9,9 +11,13 @@ export function Header() {
 
   return(
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Logo"/>
-
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="Logo"/>
+        </a>
+      </Link>
       <p>O melhor para você ouvir sempre</p>
+
       <span>{currentDate}</span>
     </header>
   );
